7843292558d1dd1feab1434afac3e99270abb10a
Adam <adam3417@gmail.com>
2022-08-05 20:12:23 -0230

    Cleaned up drawer class and added lives testing to hangman class

--- src/Hangman.java
+++ src/Hangman.java
@@ -6,6 +6,12 @@ class Hangman {
     Hangman() {
     }
     public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        Drawer.hangmanDraw(5);
+        Drawer.hangmanDraw(4);
+        Drawer.hangmanDraw(3);
+        Drawer.hangmanDraw(2);
+        Drawer.hangmanDraw(1);
+        Drawer.hangmanDraw(0);
         WordBank.main(args);
         Music.main(args);
     }
