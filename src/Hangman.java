a13d8875e9966ac06bdd2e3cdbcf287fed781bef
Adam <adam3417@gmail.com>
2022-08-05 23:21:00 -0230

    Cleaning up mains in class and using more methods

--- src/Hangman.java
+++ src/Hangman.java
@@ -8,9 +8,13 @@ class Hangman {
     }
 
     public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        // scanner for user input music class and start/stop music
         Scanner StartStop = new Scanner(System.in);
         Drawer.hangmanDraw(5);
         WordBank.main(args);
-        Music.music(StartStop);
+        Music.music(StartStop.nextLine());
+        // running the program
+        while(true) {
+        }
     }
 }
--- src/Music.java
+++ src/Music.java
@@ -11,33 +11,25 @@ public class Music {
     public Music() {
     }
 
-    public static void music(Scanner StartStop) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+    public static void music(String StartStop) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
         File soundFile = new File("src/music/background.wav");
         AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
         Clip clip = AudioSystem.getClip();
         clip.open(audioIn);
         clip.start();
-        while (StartStop.hasNext()) {
-            String command = StartStop.next();
-            if (command.equals("music")) {
-                if (clip.isRunning()) {
-                    clip.stop();
-                    System.out.println("Type 'music' to start the music.");
-                } else {
-                    System.out.println("Type 'music' to pause the music.");
-                    clip.start();
-                }
-            }
+        if (clip.isRunning()) {
+            clip.stop();
+        } else {
+            clip.start();
         }
     }
 
-    public static void soundEffect () throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        public static void soundEffect () throws LineUnavailableException, IOException, UnsupportedAudioFileException {
             File soundFile = new File("src/music/soundEffect.wav");
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioIn);
             clip.start();
+        }
     }
-
-}
 
